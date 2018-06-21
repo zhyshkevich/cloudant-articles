2c296fa390b9325d22bbbfe0dee000322667de4c
zhyshkevich <sergeyzhyshkevich@gmail.com>
2018-06-21 16:53:05 +0300

    initial commit

---
+++ src/main/java/com/test/articles/ArticlesApplication.java
@@ -0,0 +1,12 @@
+package com.test.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesApplication.class, args);
+    }
+}
